8571163d2a8942621edda6a00d67e855ebafd5f3
checksonhk <jackson.to@gmail.com>
2019-11-15 17:35:53 -0500

    set default for price

--- db/schema/05_menu_items.sql
+++ db/schema/05_menu_items.sql
@@ -7,7 +7,7 @@ CREATE TABLE widgets (
   restaurant_id INTEGER REFERENCES restaurants(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   description TEXT,
-  price INTEGER NOT NULL,
+  price INTEGER DEFAULT 0 NOT NULL ,
   time_to_prepare TIME,
   image_url VARCHAR(255),
   is_active BOOLEAN
