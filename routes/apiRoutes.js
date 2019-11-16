5b9f640709c0c1a29ad2a56284fde233e0812f67
checksonhk <jackson.to@gmail.com>
2019-11-15 19:27:05 -0500

    create template for routes and queries

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+module.exports = function(router, database) {
+ //export functions
+};
+
+router.get('/login', (req, res) => {
+  // should trigger login event
+});
+
+router.get('/home', (req, res) => {
+  res.render('home');
+});
+
+router.get('/restaurants/:id', (req, res) => {
+  res.render('restaurant');
+});
+
+router.post('/ordercart'), (req, res) => {
+}
+
+router.post('/restaurant/:id/dish/:id'), (req, res) => {
+}
+
+router.post('/checkout', (req, res) => {
+
+} = router;
+
+
