d619322a02c0bec2123369ad75705da10c79c6df
andyzen619 <andyzen619@gmail.com>
2019-11-17 07:07:29 +0000

    Added menu_item seeds for test thai restaurant

---
+++ db/seeds/03_menu_items.sql
@@ -0,0 +1,30 @@
+INSERT INTO menu_items
+(
+  restaurant_id,
+  name,
+  description,
+  price,
+  time_to_prepare,
+  image_url,
+  is_active
+)
+VALUES
+(
+  1,
+  'Thai Fried Rice',
+  'A flavorful Thai fried rice cooked in basil, chicken, and chilli peppers',
+  10.99,
+  time '00:30',
+  'https://i0.wp.com/sweetandsavorymeals.com/wp-content/uploads/2015/07/IMG_5092.jpg?w=600&ssl=1',
+  true
+),
+(
+  2,
+  'Pad Thai',
+  'A flavorful noodle dish that is cooked in basil, chicken, and bean sprouts',
+  12.99,
+  time '00:30',
+  'https://www.gimmesomeoven.com/wp-content/uploads/2019/01/Pad-Thai-Recipe-1.jpg',
+  true
+)
+
