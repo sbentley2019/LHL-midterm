626debc289c02ed31ee4868c67830acc778ad18c
checksonhk <jackson.to@gmail.com>
2019-11-17 15:05:04 -0500

    Add get all restaurant route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,26 @@
-module.exports = function(router, database) {
- //export functions
-};
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
-router.get('/login', (req, res) => {
-  // should trigger login event
-});
-
-router.get('/home', (req, res) => {
-  res.render('home');
+router.get('/', (req, res) => {
+  res.status(200);
+  res.render('index', {title: 'Ritual'});
 });
 
 router.get('/restaurants/:id', (req, res) => {
   res.render('restaurant');
 });
+
+
+/* GET restaurants from database */
+router.get('/load', (req, res) => {
+  //function.findAllRestuarants
+  .then((restaurants) => {
+    res.status(200);
+    res.json(restaurants);
+  })
+})
+
 
 router.post('/ordercart'), (req, res) => {
 }
