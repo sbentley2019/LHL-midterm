017bb24b1c792c375cc6a070fea6f89f764efd9e
sbentley2019 <sbentley000@gmail.com>
2019-11-18 17:26:57 +0000

    fix some typos

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -14,14 +14,14 @@ router.get('/restaurants/:id', (req, res) => {
   res.render('restaurant');
 });
 
-router.post('/ordercart'), (req, res) => {
-}
+router.post('/ordercart', (req, res) => {
+});
 
-router.post('/restaurant/:id/dish/:id'), (req, res) => {
-}
+router.post('/restaurant/:id/dish/:id', (req, res) => {
+});
 
 router.post('/checkout', (req, res) => {
 
-} = router;
+});
 
 
