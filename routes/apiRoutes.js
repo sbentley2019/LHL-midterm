55dc6281b27a1afd617ee491f5a4c03f84e123ef
checksonhk <jackson.to@gmail.com>
2019-11-17 22:18:38 -0500

    attach helper functions to webroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,100 +1,101 @@
-const express = require('express');
-const router = express.Router();
-const request = require('request');
+const restaurants = require('./lib/restaurants');
+const orders = require('./lib/orders');
 
-router.get('/', (req, res) => {
-  res.status(200);
-  res.render('index', {title: 'Ritual'});
-});
+module.exports = function(router, database) {
 
-router.get('/restaurants/:id', (req, res) => {
-  res.render('restaurant');
-});
-
-
-/* GET restaurants from database */
-router.get('/load_restaurants', (req, res) => {
-  //function.findAllRestuarants
-  .then((restaurants) => {
+  router.get('/', (req, res) => {
     res.status(200);
-    res.json(restaurants);
-  }).catch( (err) => {
-    console.log('ERROR: ', err);
-    throw Error('Could not get GET resturants');
-  })
-});
+    res.render('index', {title: 'Ritual'});
+  });
 
-/* GET menu_items from database from a restaurant*/
-router.get('restaurant/:id/load_menu_items', (req, res) => {
-  //function find all menu items associated to a restaurant
-  .then((menuItems) => {
-    res.status(200);
-    res.json(menuItems);
-  }).catch( (err) => {
-    console.log('ERROR: ', err);
-    throw Error('Could not get GET menu_items');
-  })
-});
+  router.get('/restaurants/:id', (req, res) => {
+    res.render('restaurant');
+  });
 
-/* GET orders for a given restaurant */
-router.get('restaurant/:id/loadOrders', (req, res) => {
-    //function to find orders by order id? or get all orders
-    .then((orders) => {
+
+  /* GET restaurants from database */
+  router.get('/load_restaurants', (req, res) => {
+    restaurants.findAllRestaurants()
+    .then((restaurants) => {
       res.status(200);
-      res.json(orders);
+      res.json(restaurants);
     }).catch( (err) => {
       console.log('ERROR: ', err);
-      throw Error('Could not get GET orders');
+      throw Error('Could not get GET resturants');
     })
-});
-
-/* GET order for a given user_id */
-router.get('/order/:id', (req, res) => {
-  //function to load current order for a given user_id
-  .then((order) => {
-    res.status(200);
-    res.json(order);
-  }).catch( (err) => {
-    console.log('ERROR: ', err);
-    throw Error('Could not get GET order');
-  })
-})
-
-/* GET menu_Items for a given order_id*/
-router.get('/order/:id/loadMenuItems', (req, res) => {
-  //function to find the menu items associated to an order id
-  .then((menu_item) => {
-    res.status(200);
-    res.json(menu_item);
-  }).catch( (err) => {
-    console.log('ERROR: ', err);
-    throw Error('Could not get GET order items');
   });
-});
 
-/* POST menu_item to current order_id */
-router.post('/order/:id/add', (req, res) => {
-  res.status(200);
-  const {
-    name,
-    description,
-    price,
-    img_url,
-    quantity
-  };
-  //function to insert menu_item to an order_id
-  .then((res) => {
-    console.log('success')
-  }).catch((err) => [
-    console.log("failed", err)
-  ])
-})
+  /* GET menu_items from database from a restaurant*/
+  router.get('restaurant/:id/load_menu_items', (req, res) => {
+    restaurants.findAllMenuItemsForRestaurants(1) // using restaurant 1 to test
+    .then((menuItems) => {
+      res.status(200);
+      res.json(menuItems);
+    }).catch( (err) => {
+      console.log('ERROR: ', err);
+      throw Error('Could not get GET menu_items');
+    })
+  });
 
-router.post('/restaurant/:id/dish/:id'), (req, res) => {
-}
+  /* GET orders for a given restaurant */
+  router.get('restaurant/:id/loadOrders', (req, res) => {
+      orders.findByRestaurant(1) // using restaurant 1 to test
+      .then((orders) => {
+        res.status(200);
+        res.json(orders);
+      }).catch( (err) => {
+        console.log('ERROR: ', err);
+        throw Error('Could not get GET orders');
+      })
+  });
 
-router.post('/checkout', (req, res) => {
+  /* GET order for a given user_id */
+  router.get('/order/:id', (req, res) => {
+    orders.findByUserId()
+    .then((order) => {
+      res.status(200);
+      res.json(order);
+    }).catch( (err) => {
+      console.log('ERROR: ', err);
+      throw Error('Could not get GET order');
+    })
+  })
 
-module.exports = router;
+  /* GET menu_Items for a given order_id*/
+  router.get('/order/:id/loadMenuItems', (req, res) => {
+    //function to find the menu items associated to an order id
+    .then((menu_item) => {
+      res.status(200);
+      res.json(menu_item);
+    }).catch( (err) => {
+      console.log('ERROR: ', err);
+      throw Error('Could not get GET order items');
+    });
+  });
 
+  /* POST menu_item to current order_id */
+  router.post('/order/:id/add', (req, res) => {
+    res.status(200);
+    const {
+      name,
+      description,
+      price,
+      img_url,
+      quantity
+    };
+    //function to insert menu_item to an order_id
+    .then((res) => {
+      console.log('success')
+    }).catch((err) => [
+      console.log("failed", err)
+    ])
+  })
+
+  router.post('/restaurant/:id/dish/:id', (req, res) => {
+  });
+
+  router.post('/checkout', (req, res) => {});
+
+  return router;
+};
 
--- server.js
+++ server.js
@@ -34,25 +34,27 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
+const apiRoutes = require('./routes/apiRoutes.js');
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
+/* /api/endpoints/ */
 
-// Home page
-// Warning: avoid creating more routes in this file!
-// Separate them into separate routes files (see above).
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use('/api', apiRoutes(db));
 
 app.get("/restaurant", (req, res) => {
   res.render("restaurant");
 });
+
+// Home page
+// Warning: avoid creating more routes in this file!
+// Separate them into separate routes files (see above).
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
