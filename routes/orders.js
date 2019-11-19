0176241000ee90b8b0286d688be789d1db9804c2
checksonhk <jackson.to@gmail.com>
2019-11-18 22:26:14 -0500

    Add ajax to add menu_items to ordersitem

--- routes/orders.js
+++ routes/orders.js
@@ -1,20 +1,48 @@
 const express = require('express');
 const router  = express.Router();
+const orders = require('../lib/database/orders');
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
+  /* GET order for a given user_id */
+  router.get('/:id', (req, res) => {
+    orders.findByUserId()
+      .then((order) => {
+        res.status(200);
+        res.json(order);
+      }).catch((err) => {
+        console.log('ERROR: ', err);
+        throw Error('Could not get GET order');
       });
+  });
+
+  /* GET menu_Items for a given order_id*/
+  router.get('/:id/loadMenuItems', (req, res) => {
+    orders.findAllMenuItemsForOrder(req.params)
+      .then((menu_item) => {
+        res.status(200);
+        res.json(menu_item);
+      }).catch((err) => {
+        console.log('ERROR: ', err);
+        throw Error('Could not get GET order items');
+      });
+  });
+
+  router.post('/:id', (req, res) => {
+    res.status(200);
+    orders.addOrderItem(req.session.order_id, req.body.menu_item).then((res) => {
+      console.log('success');
+    }).catch(err => {
+      console.log('failed', err);
+    });
+  });
+
+  /* POST menu_item to current order_id */
+  router.post('/:id/add', (req, res) => {
+    res.status(200);
+    orders.updateOrderItem(req.session.order_id, req.params.body.menu_item).then(res => {
+      console.log('success');
+    });
+
   });
   return router;
 };
