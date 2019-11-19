660f90353a34734c7214251b1ddec601cf3282fc
checksonhk <jackson.to@gmail.com>
2019-11-18 23:18:01 -0500

    now checks if ordertiem exist and adds 1 for every add button clicked

---
+++ public/scripts/order.js
@@ -0,0 +1,16 @@
+const submitOrderItem = function(url, data) {
+  return $.ajax({
+    method: "POST",
+    url: url,
+    data,
+  });
+};
+
+$(() => {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    const url = this.action;
+    const data = $(this).serialize();
+    submitOrderItem(url, data).then(res => console.log('success'));
+  });
+});
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -35,59 +35,16 @@ module.exports = function(database) {
       .then((orders) => {
         res.status(200);
         res.json(orders);
-      }).catch( (err) => {
+      }).catch((err) => {
         console.log('ERROR: ', err);
         throw Error('Could not get GET orders');
       });
   });
 
-  /* GET order for a given user_id */
-  router.get('/order/:id', (req, res) => {
-    orders.findByUserId()
-      .then((order) => {
-        res.status(200);
-        res.json(order);
-      }).catch((err) => {
-        console.log('ERROR: ', err);
-        throw Error('Could not get GET order');
-      })
-  })
-
-  /* GET menu_Items for a given order_id*/
-  router.get('/order/:id/loadMenuItems', (req, res) => {
-    orders.findAllMenuItemsForOrder(req.params)
-      .then((menu_item) => {
-        res.status(200);
-        res.json(menu_item);
-      }).catch((err) => {
-        console.log('ERROR: ', err);
-        throw Error('Could not get GET order items');
-      });
-  });
-
-  /* POST menu_item to current order_id */
-  router.post('/order/:id/add', (req, res) => {
-    res.status(200);
-    const {
-      name,
-      description,
-      price,
-      img_url,
-      quantity
-    } = req.params;
-    //function to insert menu_item to an order_id
-    orders.findAllMenuItemsForOrder(req.params)
-      .then((res) => {
-        console.log('success');
-      }).catch((err) => [
-        console.log("failed", err)
-      ]);
-  });
 
   router.post('/restaurant/:id/dish/:id', (req, res) => {
   });
 
-  router.post('/checkout', (req, res) => {});
 
   return router;
 };
