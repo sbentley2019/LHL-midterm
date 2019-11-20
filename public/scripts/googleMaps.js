93f9ab6f1a8a289e549a98a8e6893e48e3292ba2
sbentley2019 <sbentley000@gmail.com>
2019-11-20 16:31:16 +0000

    Add BOUNCE animation to map markers

--- public/scripts/googleMaps.js
+++ public/scripts/googleMaps.js
@@ -1,13 +1,17 @@
 $(() => {
+  let map;
+  const restMark = {};
+
   // This function uses the google maps api and the location is centered around lat 43.644, long -79.402
   function initMap() {
+
     let coordinates = {lat: 43.644, lng: -79.402};
-    let map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById('map'), {
       center: coordinates,
-      zoom: 17
+      zoom: 17,
+      gestureHandling: 'cooperative'
     });
 
-    const restMark = {};
     // This puts all the restaurants as markers on the map
     $('.restaurant-cards').each(function() {
       let coordinates = {lat: Number($(this).attr('data-lat')), lng: Number($(this).attr('data-lng')) };
@@ -16,10 +20,20 @@ $(() => {
       restMark[restaurant_id] = new google.maps.Marker({
         position: coordinates,
         map: map,
+        size: new google.maps.Size(15, 15),
         animation: google.maps.Animation.DROP,
-        label: restaurant_id});
-    });
-  };
+        icon: `http://thydzik.com/thydzikGoogleMap/markerlink.php?text=${Number(restaurant_id)}&color=5680FC&image=.png`,
+        title:$(this).attr("data-name")});
+      });
+    };
+
+  $('.restaurant-cards').on('mouseenter', function() {
+    let restaurant_id = ($(this).attr('href')).split('/')[2];
+    restMark[restaurant_id].setAnimation(google.maps.Animation.BOUNCE);
+  }).on('mouseleave', function() {
+    let restaurant_id = ($(this).attr('href')).split('/')[2];
+    restMark[restaurant_id].setAnimation(null);
+  });
 
   //This loads the google map when the home-page is finished loaded
   window.onload = function() {
