4980f67529bfe8f904da3145780259744bebc274
checksonhk <jackson.to@gmail.com>
2019-11-20 13:50:57 -0500

    fix merge conflicts

--- public/scripts/restaurants.js
+++ public/scripts/restaurants.js
@@ -1,10 +1,6 @@
 
-
 $(() => {
-  $("form").submit(function(event) {
-    event.preventDefault();
-    const url = this.action;
-    const data = $(this).serialize();
-    submitOrderItem(url, data).then(res => console.log('success'));
+  $("a.restaurant-cards").submit(function(event) {
+    console.log(this);
   });
 });
