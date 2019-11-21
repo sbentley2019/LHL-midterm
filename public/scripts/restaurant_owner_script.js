be541ff049abda55956d6fefafb879ebebf37a45
andyzen619 <andyzen619@gmail.com>
2019-11-21 04:20:02 +0000

    Added reactive pending and active orders

--- public/scripts/restaurant_owner_script.js
+++ public/scripts/restaurant_owner_script.js
@@ -73,16 +73,70 @@ const generateOrder = function(order, orderDetails) {
   return orderBody;
 };
 
+const generatePendingOrder = function(order) {
+  let orderBody = $(`
+  <div class="cell">
+                    <div class="card" style='padding: 0%;'>
+                        <div class="card-divider">
+                            <div class="owner_order_card_header">
+                                <h4>Order #${order.id}</h4>
+                                <h6>Estimated Cook time: 1 hr</h6>
+                            </div>
+                            <div style="margin-left: auto; margin-top: auto">
+                            </div>
+                        </div>
+                        <div class="owner_order_card_body">
+                            <ul class="vertical menu" style="margin: 25px;" id="pendingOrderBody">
+
+                            </ul>
+                        </div>
+                        <div class="card-divider owner_order_card_footer">
+                            <span style="width: 60%; margin: auto">
+                                    <div>
+                                        <div class="slider" data-slider data-initial-start="50" data-step="5">
+
+                            <span class="slider-fill" data-slider-fill></span>
+                        </div>
+
+                    </div>
+                    <div class="cell small-2">
+                        <input type="number" id="sliderOutput2">
+                    </div>
+                    </span>
+
+                    <div>
+                        <form action="/owner/confirm_order" method="POST">
+                            <button class="button" type="submit">Accept</button>
+                        </form>
+                        <form action="/owner/cancel_order" method="POST">
+                            <button class="button" type="submit">Cancel</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>`);
+  return orderBody;
+};
+
 $(() => {
   clearRenderOrderItem('ordersGrid');
   fetchOrderList().then(orderList => {
     for (const order of orderList.orderItems) {
       console.log(order);
-      $("#ordersGrid").prepend(generateOrder(order));
-      retrieveMenuItem(order.id).then(orderItemList => {
-        console.log(orderItemList);
-        $("#order-body").prepend(generateOrderItemsList(orderItemList));
-      })
+      if (order.current_status !== 'Pending') {
+        $("#ordersGrid").prepend(generateOrder(order));
+        retrieveMenuItem(order.id).then(orderItemList => {
+          console.log(orderItemList);
+          $("#order-body").prepend(generateOrderItemsList(orderItemList));
+        })
+      } else {
+        console.log('pending order');
+        $("#pendingOrdersGrid").prepend(generatePendingOrder(order));
+        retrieveMenuItem(order.id).then(orderItemList => {
+          console.log(orderItemList);
+          $("#pendingOrderBody").prepend(generateOrderItemsList(orderItemList));
+        });
+      }
     }
   })
 });
